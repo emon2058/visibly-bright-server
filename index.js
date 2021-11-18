746d6d9549fa05639facad0b029cbfc6342e53f0
emon2058 <ashad2058@gmail.com>
2021-11-19 05:05:23 +0600

    remove firebase admin

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const admin = require("firebase-admin");
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
-
-const serviceAccount = require('./visibly-bright-firebase-adminsdk-55olr-193131d9c5.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
 
 // middleware 
 app.use(cors());
@@ -19,20 +12,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l2twi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-async function verifyToken(req,res,next){
-    if(req.headers.authorization){
-        const token = req.headers.authorization.split(' ')[1];
-        try{
-            const decodedUser = await admin.auth().verifyIdToken(token);
-            req.decodedEmail = decodedUser.email;
-        }
-        catch{
-
-        }
-    }
-    next()
-}
 
 async function run(){
     try{
@@ -108,21 +87,12 @@ async function run(){
             const result = await usersCollection.updateOne(filter,updateDoc,options);
             res.json(result)
         })
-        app.put('/users/admin',verifyToken,async(req,res)=>{
+        app.put('/users/admin',async(req,res)=>{
             const user = req.body;
-            const requester = req.decodedEmail;
-            if(requester){
-                const requesterAccount = usersCollection.findOne({email:requester});
-                if(requesterAccount.role === 'admin'){
-                    const filter = {email:user.email};
-                    const updateDoc = {$set:{role:'admin'}};
-                    const result = await usersCollection.updateOne(filter,updateDoc);
-                    res.json(result)
-                }
-            }
-            else{
-                res.status(403).json({message: 'you do not have access to make admin'})
-            }
+            const filter = {email:user.email};
+            const updateDoc = {$set:{role:'admin'}};
+            const result = await usersCollection.updateOne(filter,updateDoc);
+            res.json(result)
         })
 
         app.post('/orders',async(req,res)=>{
